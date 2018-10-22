93d2589cb6e70ef16bf2e24542c51c9446c71d86
Utku Eray <>
2018-10-23 00:41:59 +0300

    no message

--- Bloxorz.py
+++ Bloxorz.py
@@ -65,14 +65,14 @@ class Bloxorz:
             print(self.board[y])
 
     def checkTheTile(self, x, y):
-        #index out of range error
 
-        if self.board[y][x] == "X":
+        if self.xLen > x >= 0:
+            if self.yLen > y >= 0:
+                if self.board[y][x] == "X":
+                    return False
+                else:
+                    return True
             return False
-        elif self.board[y][x] == "G" and self.player.state == 0:
-            print("Goal")
-        else:
-            return True
 
     def checkState(self):
         if self.blockPoint == self.blockPoint:
@@ -106,154 +106,6 @@ class Bloxorz:
         self.board[self.player.currentPoint2[1]] = "".join(cleanBoard2)
         self.board[self.goalPoint[1]] = "".join(cleanBoard3)
 
-    def move(self, direction):
-        x1 = self.player.currentPoint[0]
-        y1 = self.player.currentPoint[1]
-        x2 = self.player.currentPoint2[0]
-        y2 = self.player.currentPoint2[1]
-
-        if self.player.state == 0:  # vertical
-            if direction == "U" or direction == "D":  # player state will be 2
-                if direction == "U":
-                    print("moving up")
-                    y1 -= 1
-                    y2 -= 2
-
-                elif direction == "D":
-                    print("moving down")
-                    y1 += 1
-                    y2 += 2
-
-                if self.checkTheTile(x1, y1):
-                    if self.checkTheTile(x2, y2):
-                        result = [[x1, y1], [x2, y2], 2]
-
-            if direction == "L" or direction == "R":  # player state will be 1
-                if direction == "R":
-                    print("moving right")
-                    x1 += 1
-                    x2 += 2
-
-                elif direction == "L":
-                    print("movign left")
-                    x1 -= 1
-                    x2 -= 2
-
-                if self.checkTheTile(x1, y1):
-                    if self.checkTheTile(x2, y2):
-                        result = [[x1, y1], [x2, y2], 1]
-
-        elif self.player.state == 1:  # horizontal 1
-            if direction == "U" or direction == "D":  # player state will be 1
-                if direction == "U":
-                    print("moving up")
-                    y1 -= 1
-                    y2 -= 1
-
-                elif direction == "D":
-                    print("moving down")
-                    y1 += 1
-                    y2 += 1
-
-                if self.checkTheTile(x1, y1):
-                    if self.checkTheTile(x2, y2):
-                        result = [[x1, y1], [x2, y2], 1]
-
-            if direction == "L" or direction == "R":  # player state will be 0
-                if direction == "R":
-                    print("moving right")
-                    if x1 > x2:
-                        x1 += 1
-                        x2 = x1
-                    else:
-                        x2 += 1
-                        x1 = x2
-
-                elif direction == "L":
-                    print("moving left")
-                    if x1 < x2:
-                        x1 -= 1
-                        x2 = x1
-                    else:
-                        x2 -= 1
-                        x1 = x2
-
-                if self.checkTheTile(x1, y1):
-                    if self.checkTheTile(x2, y2):
-                        result = [[x1, y1], [x2, y2], 0]
-
-        elif self.player.state == 2:  # horizontal 2
-            if direction == "U" or direction == "D":  # player state will be 1
-                if direction == "U":
-                    print("moving up")
-                    if y1 < y2:
-                        y1 -= 1
-                        y2 = y1
-                    else:
-                        y2 -= 1
-                        y1 = y2
-
-                elif direction == "D":
-                    print("moving down")
-
-                    if y1 > y2:
-                        y1 += 1
-                        y2 = y1
-                    else:
-                        y2 += 1
-                        y1 = y2
-
-                if self.checkTheTile(x1, y1):
-                    if self.checkTheTile(x2, y2):
-                        result = [[x1, y1], [x2, y2], 0]
-
-            if direction == "L" or direction == "R":  # player state will be 0
-                if direction == "R":
-                    print("moving right")
-                    x1 += 1
-                    x2 += 1
-
-                elif direction == "L":
-                    print("moving left")
-                    x1 -= 1
-                    x2 -= 1
-
-                if self.checkTheTile(x1, y1):
-                    if self.checkTheTile(x2, y2):
-                        result = [[x1, y1], [x2, y2], 2]
-
-        return result
-
-    def movePlayer(self, direction):
-        self.clearOldPoints()
-
-        print("---------------")
-
-        self.move(direction)
-
-        if self.player.state == 0:
-            print("Block At: ", self.player.currentPoint)
-        else:
-            print("Block At: ", self.player.currentPoint, self.player.currentPoint2)
-
-        self.placeNewPoints()
-        print()
-        self.printBoard()
-
-        if self.player.state == 0 and self.player.currentPoint == self.goalPoint:
-            print("The End")
-
-    def hashx(self, obj):
-
-        coor1x = obj[0][0]
-        coor1y = obj[0][1]
-        coor2x = obj[1][0]
-        coor2y = obj[1][1]
-
-        hashx = ((len(self.board) * len(self.board[0]) * obj[2]) + (len(self.board[0]) * coor1y)) + coor2x
-
-        return hashx
-
     def neighbors(self, current):
 
         """
@@ -270,71 +122,82 @@ class Bloxorz:
 
         if current[2] == 0:  # state = 0
             # check up
-            if self.checkTheTile(coor1x, coor1y - 1):
-                if self.checkTheTile(coor2x, coor2y - 2):
-                    result.append([[coor1x, coor1y - 1], [coor2x, coor2y - 2], 2])
+            if self.checkTheTile(coor1x, coor1y - 1) and self.checkTheTile(coor2x, coor2y - 2):
+                result.append([[coor1x, coor1y - 1], [coor2x, coor2y - 2], 2])
 
             # check down
-            if self.checkTheTile(coor1x, coor1y + 1):
-                if self.checkTheTile(coor2x, coor2y + 2):
-                    result.append([[coor1x, coor1y + 1], [coor2x, coor2y + 2], 2])
+            if self.checkTheTile(coor1x, coor1y + 1) and self.checkTheTile(coor2x, coor2y + 2):
+                result.append([[coor1x, coor1y + 1], [coor2x, coor2y + 2], 2])
 
             # check left
-            if self.checkTheTile(coor1x - 1, coor1y):
-                if self.checkTheTile(coor2x - 2, coor2y):
-                    result.append([[coor1x - 1, coor1y], [coor2x - 2, coor2y], 1])
+            if self.checkTheTile(coor1x - 1, coor1y) and self.checkTheTile(coor2x - 2, coor2y):
+                result.append([[coor1x - 1, coor1y], [coor2x - 2, coor2y], 1])
 
             # check right
-            if self.checkTheTile(coor1x + 1, coor1y):
-                if self.checkTheTile(coor2x + 2, coor2y):
-                    result.append([[coor1x + 1, coor1y], [coor2x + 2, coor2y], 1])
+            if self.checkTheTile(coor1x + 1, coor1y) and self.checkTheTile(coor2x + 2, coor2y):
+                 result.append([[coor1x + 1, coor1y], [coor2x + 2, coor2y], 1])
 
         elif current[2] == 1:  # state = 1
             # check up
-            if self.checkTheTile(coor1x, coor1y - 1):
-                if self.checkTheTile(coor2x, coor2y - 1):
-                    result.append([[coor1x, coor1y - 1], [coor2x, coor2y - 1], 1])
+            if self.checkTheTile(coor1x, coor1y - 1) and self.checkTheTile(coor2x, coor2y - 1):
+                 result.append([[coor1x, coor1y - 1], [coor2x, coor2y - 1], 1])
 
             # check down
-            if self.checkTheTile(coor1x, coor1y + 1):
-                if self.checkTheTile(coor2x, coor2y + 1):
-                    result.append([[coor1x, coor1y + 1], [coor2x, coor2y + 1], 1])
+            if self.checkTheTile(coor1x, coor1y + 1) and self.checkTheTile(coor2x, coor2y + 1):
+                result.append([[coor1x, coor1y + 1], [coor2x, coor2y + 1], 1])
 
-            # check left
-            if self.checkTheTile(coor1x - 1, coor1y):
-                if self.checkTheTile(coor2x - 2, coor2y):
+            if coor2x > coor1x:
+                # check left
+                if self.checkTheTile(coor1x - 1, coor1y) and self.checkTheTile(coor2x - 2, coor2y):
                     result.append([[coor1x - 1, coor1y], [coor2x - 2, coor2y], 0])
 
-            # check right
-            if self.checkTheTile(coor1x + 1, coor1y):
-                if self.checkTheTile(coor2x + 2, coor2y):
+                # check right
+                if self.checkTheTile(coor1x + 2, coor1y) and self.checkTheTile(coor2x + 1, coor2y):
+                    result.append([[coor1x + 2, coor1y], [coor2x + 1, coor2y], 0])
+
+            else:
+
+                # check left
+                if self.checkTheTile(coor1x - 2, coor1y) and self.checkTheTile(coor2x - 1, coor2y):
+                    result.append([[coor1x - 2, coor1y], [coor2x - 1, coor2y], 0])
+
+                # check right
+                if self.checkTheTile(coor1x + 1, coor1y) and self.checkTheTile(coor2x + 2, coor2y):
                     result.append([[coor1x + 1, coor1y], [coor2x + 2, coor2y], 0])
 
         elif current[2] == 2:  # state = 2
-            # check up
-            if self.checkTheTile(coor1x, coor1y - 1):
-                if self.checkTheTile(coor2x, coor2y - 2):
+
+            if coor2y > coor1y:
+                # check up
+                if self.checkTheTile(coor1x, coor1y - 1) and self.checkTheTile(coor2x, coor2y - 2):
                     result.append([[coor1x, coor1y - 1], [coor2x, coor2y - 2], 0])
 
-            # check down
-            if self.checkTheTile(coor1x, coor1y + 1):
-                if self.checkTheTile(coor2x, coor2y + 2):
+                # check down
+                if self.checkTheTile(coor1x, coor1y + 1) and self.checkTheTile(coor2x, coor2y + 2):
                     result.append([[coor1x, coor1y + 1], [coor2x, coor2y + 2], 0])
+
+            else:
+
+                # check up
+                if self.checkTheTile(coor1x, coor1y - 2) and self.checkTheTile(coor2x, coor2y - 1):
+                    result.append([[coor1x, coor1y - 2], [coor2x, coor2y - 1], 0])
+
+                # check down
+                if self.checkTheTile(coor1x, coor1y + 2) and self.checkTheTile(coor2x, coor2y + 1):
+                    result.append([[coor1x, coor1y + 2], [coor2x, coor2y + 1], 0])
+
             # check left
-            if self.checkTheTile(coor1x - 1, coor1y):
-                if self.checkTheTile(coor2x - 1, coor2y):
-                    result.append([[coor1x - 1, coor1y], [coor2x - 1, coor2y], 2])
+            if self.checkTheTile(coor1x - 1, coor1y) and self.checkTheTile(coor2x - 1, coor2y):
+               result.append([[coor1x - 1, coor1y], [coor2x - 1, coor2y], 2])
 
             # check right
-            if self.checkTheTile(coor1x + 1, coor1y):
-                if self.checkTheTile(coor2x + 1, coor2y):
-                    result.append([[coor1x + 1, coor1y], [coor2x + 1, coor2y], 2])
+            if self.checkTheTile(coor1x + 1, coor1y) and self.checkTheTile(coor2x + 1, coor2y):
+                result.append([[coor1x + 1, coor1y], [coor2x + 1, coor2y], 2])
 
-        if current[0] == self.goalPoint and current[1] == self.goalPoint:
-        print(result)
         return result
 
     def UCS(self):
+
         print("---------------")
         self.findStart()
         print()
@@ -349,27 +212,33 @@ class Bloxorz:
         totalCost = {str(start): 0}
 
         queue.put(start, 0)  # start, total cost
+
         while not queue.empty():
             current = queue.get()
 
             if current == goal:
                 print("Finished")
+                print(current)
+                for i in totalCost:
+                    print(i)
+
                 self.printBoard()
                 break
 
             for next in self.neighbors(current):
-                self.printBoard()
+
                 print()
                 newCost = totalCost[str(current)] + 1
-                print(newCost)
 
                 if str(next) not in totalCost and str(next) not in visited or newCost < totalCost[str(next)]:
                     totalCost[str(next)] = newCost
                     priority = newCost
                     queue.put(next, priority)
                     crt = str(current)
-                    visited.append(str(next))
-                    print()
+                    visited.append(crt)
                     self.clearOldPoints()
                     self.player.updatePlayer(next[0], next[1], next[2])
+                    print(newCost)
+                    print(next)
                     self.placeNewPoints()
+                    self.printBoard()
